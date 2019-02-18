8dc8cbfeba37d47f740c7e96fb22108bbe3d4923
Chedak <cedomir.miskov@gmail.com>
2019-02-18 12:01:41 +0100

    Add answers to 2nd tutorial

--- Tutorial & quiz 2.sql
+++ Tutorial & quiz 2.sql
@@ -30,3 +30,44 @@ WHERE area > 3000000 OR population > 250000000
 -- 8 One or the other (but not both)
 SELECT name, population, area FROM world
 WHERE area > 3000000 XOR population > 250000000
+
+
+-- 9 Rounding
+SELECT name,
+ROUND(population/1000000, 2) AS population_rounded,
+ROUND(gdp/1000000000, 2) AS GDP_rounded
+  FROM  world
+  WHERE continent = 'South America'
+;
+
+
+-- 10 Trillion dollar economies
+SELECT name, ROUND((gdp/population)/1000)*1000 AS 'GDP/pop' FROM world
+
+WHERE gdp > 1000000000000
+;
+
+
+-- 11 Name and capital have the same length
+SELECT name, capital
+  FROM world
+ WHERE LENGTH(name)  =  LENGTH(capital)
+;
+
+-- 12 Matching name and capital
+SELECT name, capital
+  FROM world
+ WHERE LEFT(name,1)  =  LEFT(capital,1)
+AND name NOT LIKE capital
+;
+
+-- 13 All the vowels
+SELECT name
+   FROM world
+WHERE name LIKE ('%a%')
+AND name LIKE ('%e%')
+AND name LIKE ('%i%')
+AND name LIKE ('%o%')
+AND name LIKE ('%u%')
+AND name NOT LIKE ('% %')
+;
