6bc7bc17e0ee06ab4813f25e8f2849a4b1c3d644
Chedak <cedomir.miskov@gmail.com>
2019-02-20 15:49:49 +0100

    Add tutorial answers for file no7

--- Tutorial & quiz 7.sql
+++ Tutorial & quiz 7.sql
@@ -8,7 +8,7 @@ SELECT id, title
  FROM movie
  WHERE title='Citizen Kane'
 
- --3 
+ --3
  SELECT id, title, yr
  FROM movie
  WHERE title LIKE '%Star Trek%'
@@ -33,3 +33,74 @@ SELECT id
  ON actor.id=actorid
 WHERE movieid=11768
 
+-- 7
+SELECT name
+FROM movie
+  JOIN casting
+    ON movie.id=movieid
+  JOIN actor
+    ON actor.id=actorid
+WHERE movie.title = 'Alien'
+;
+
+-- 8
+SELECT movie.title FROM movie
+  JOIN casting
+  ON movie.id = casting.movieid
+  JOIN actor
+  ON casting.actorid = actor.id
+WHERE actor.name = 'Harrison Ford'
+;
+
+-- 9
+SELECT movie.title FROM movie
+  JOIN casting
+  ON movie.id = casting.movieid
+  JOIN actor
+  ON casting.actorid = actor.id
+WHERE actor.name = 'Harrison Ford'
+AND casting.ord > 1
+;
+
+-- 10
+SELECT movie.title, actor.name FROM movie
+  JOIN casting
+  ON movie.id = casting.movieid
+  JOIN actor
+  ON casting.actorid = actor.id
+WHERE movie.yr = 1962
+AND casting.ord = 1
+;
+
+-- 11
+SELECT yr,COUNT(title)
+FROM movie
+  JOIN casting
+    ON movie.id=movieid
+  JOIN actor
+    ON actorid=actor.id
+WHERE name='John Travolta'
+GROUP BY yr
+
+HAVING
+  COUNT(title)=
+  (SELECT MAX(c) FROM
+    (SELECT yr, COUNT(title) AS c
+      FROM movie
+        JOIN casting  ON movie.id=movieid
+        JOIN actor     ON actorid=actor.id
+        WHERE name='John Travolta'
+        GROUP BY yr)
+   AS t)
+;
+
+
+-- 12
+SELECT new.title, new.ord FROM
+(SELECT movie.title AS t, actor.name, casting.ord AS o
+  FROM movie
+  JOIN casting
+     ON movie.id = casting.movieid
+  JOIN actor
+     ON casting.actorid = actor.id AND actor.name = 'Julie Andrews') AS new
+WHERE new.ord=1
