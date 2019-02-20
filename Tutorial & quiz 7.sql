d53cb3b2918efbe188ed5b5de72eb57e918131f6
Alex N <nikitin2009@gmail.com>
2019-02-20 16:25:24 +0200

    Add ques

---
+++ Tutorial & quiz 7.sql
@@ -0,0 +1,35 @@
+--1
+SELECT id, title
+ FROM movie
+ WHERE yr=1962
+
+ --2
+ SELECT yr
+ FROM movie
+ WHERE title='Citizen Kane'
+
+ --3 
+ SELECT id, title, yr
+ FROM movie
+ WHERE title LIKE '%Star Trek%'
+ORDER BY yr
+
+--4
+SELECT id
+ FROM actor
+ WHERE name='Glenn Close'
+
+ --5
+ SELECT id
+ FROM movie
+ WHERE title='Casablanca'
+
+ --6
+ SELECT name
+ FROM movie
+ JOIN casting
+ ON movie.id=movieid
+ JOIN actor
+ ON actor.id=actorid
+WHERE movieid=11768
+
