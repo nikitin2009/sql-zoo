02fbc7f811b016d23ad85967b0da29d13e414bf2
Alex N <nikitin2009@gmail.com>
2019-02-21 13:30:56 +0200

    Add answers

--- Tutorial & quiz 9.sql
+++ Tutorial & quiz 9.sql
@@ -48,3 +48,27 @@ WHERE stopa.name='Craiglockhart' AND stopb.name = 'London Road'
 ;
 
 -- 7 Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith')
+SELECT DISTINCT a.company, a.num
+  FROM route a
+  JOIN route b
+    ON (a.company=b.company AND a.num=b.num)
+
+WHERE a.stop=115 AND b.stop = 137
+
+-- 8
+SELECT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' AND stopb.name='Tollcross'
+
+-- 9
+SELECT DISTINCT stopb.name, b.company, b.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart'
+
+-- 10
